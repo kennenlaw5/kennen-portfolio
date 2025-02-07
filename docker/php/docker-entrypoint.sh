2b54998e0e52ae823840540600e143aac024bbc8
Kennen <kennenlaw5@gmail.com>
2025-02-07 16:03:13 -0700

    Check for nginx instance in render

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Start Nginx in the background and keep it there
-nginx -g 'daemon off;' &
-nginx_pid=$!
+# nginx -g 'daemon off;' &
+# nginx_pid=$!
 
 # Start PHP-FPM (and pass control to it)
 exec "$@"
 
 # This part should ideally never be reached, but it's a safeguard
-wait "$nginx_pid"
+# wait "$nginx_pid"
