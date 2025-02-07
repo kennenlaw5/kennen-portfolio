b61b857ca86b7665155da4778067e1a8119e7c04
Kennen <kennenlaw5@gmail.com>
2025-02-07 14:09:49 -0700

    Add strace

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 set -e
 
-sleep 5
-
 # Start Nginx in the background and keep it there
 nginx -g 'daemon off;' &
 nginx_pid=$!
 
-# Start PHP-FPM (and pass control to it)
-exec "$@"
+# Start PHP-FPM with strace
+exec strace -f -o /var/log/php-fpm_strace.log php-fpm
 
 # This part should ideally never be reached, but it's a safeguard
 wait "$nginx_pid"
