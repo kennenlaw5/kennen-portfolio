afce65b9cc9b34b04e712744ca5144feab9d2078
Kennen <kennenlaw5@gmail.com>
2025-02-07 14:37:42 -0700

    Remove strace

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 nginx -g 'daemon off;' &
 nginx_pid=$!
 
-# Start PHP-FPM with strace
-exec strace -f -o /var/log/php-fpm_strace.log php-fpm
+# Start PHP-FPM (and pass control to it)
+exec "$@"
 
 # This part should ideally never be reached, but it's a safeguard
 wait "$nginx_pid"
